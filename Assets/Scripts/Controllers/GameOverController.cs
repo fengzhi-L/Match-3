4b7fd78a2982f1f89207f31886f396f5d0e7439f
fengzhi-L <bobliang001@gmail.com>
2026-01-21 11:55:02 +0800

    Update GameOverController.cs
    实现结算面板显示跳转等逻辑

--- Assets/Scripts/Controllers/GameOverController.cs
+++ Assets/Scripts/Controllers/GameOverController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using QFramework;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour, IController
@@ -14,6 +16,77 @@ public class GameOverController : MonoBehaviour, IController
     private void Start()
     {
         screenParent.SetActive(false);
+
+        foreach (var star in stars)
+        {
+            star.enabled = false;
+        }
+
+        this.RegisterEvent<GameWinEvent>(e => { ShowWin(e.Score, e.StarCount); });
+        this.RegisterEvent<GameLoseEvent>(e => { ShowLose(); });
+    }
+
+    public void ShowLose()
+    {
+        screenParent.SetActive(true);
+        scoreParent.SetActive(false);
+
+        var anim = GetComponent<Animator>();
+
+        if (anim)
+        {
+            anim.Play("GameOverShow");
+        }
+    }
+
+    public void ShowWin(int score, int starCount)
+    {
+        screenParent.SetActive(true);
+        loseText.enabled = false;
+
+        scoreText.text = score.ToString();
+        scoreText.enabled = false;
+        
+        var anim = GetComponent<Animator>();
+
+        if (anim)
+        {
+            anim.Play("GameOverShow");
+        }
+
+        StartCoroutine(ShowWinCoroutine(starCount));
+    }
+
+    private IEnumerator ShowWinCoroutine(int starCount)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (starCount < stars.Length)
+        {
+            for (var i = 0; i < starCount; i++)
+            {
+                stars[i].enabled = true;
+
+                if (i > 0)
+                {
+                    stars[i - 1].enabled = false;
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+        scoreText.enabled = true;
+    }
+
+    public void OnReplayClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnDoneClicked()
+    {
+        SceneManager.LoadScene("LevelSelect");
     }
 
     public IArchitecture GetArchitecture()
